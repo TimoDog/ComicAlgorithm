eb8470a1ff89fb63f744c46fa29c56cc9f82a2f3
TimoDog <37979663+TimoDog@users.noreply.github.com>
2019-08-01 16:09:44 +0800

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,20 @@
+package comicAlgorithm.linkedList;
+
+/**
+ * @Author: TimoDog email:ax9nvm@Outlook.com
+ * @Date: 2019/8/1 12:14
+ */
+
+/**
+ * 链表节点
+ */
+
+public class Node {
+
+    int data;
+    Node next;
+    Node(int data) {
+        this.data = data;
+    }
+
+}
